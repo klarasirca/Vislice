2a689ba4f4d8353177ca250b8d322c05a1398fdb
Klara Širca <klara.sirca@student.fmf.uni-lj.si>
2019-05-14 19:29:41 +0200

    naredili vislice

--- model.py
+++ model.py
@@ -9,54 +9,54 @@ PORAZ = 'x'
 
 class Igra:
     def __init__(self, geslo, crke=[]):
-        self.geslo = geslo.upper
+        self.geslo = geslo.upper()
         self.crke = crke 
 
-def pravilne_crke(self):
+    def pravilne_crke(self):
  #vrne tiste crke ce so v geslu
-    return[crka for crka in self.crke if crka in self.geslo]
+        return[crka for crka in self.crke if crka in self.geslo]
 
-def napacne_crke(self):
-    return[crka for crka in self.crke if crka not in self.geslo]
+    def napacne_crke(self):
+        return[crka for crka in self.crke if crka not in self.geslo]
 
-def stevilo_napak(self):
-    return len(self.napacne_crke())
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
 
-def zmaga(self):
-    for crka in self.geslo:
-        if crka not in self.crke:
-            return False
-    return True
+    def zmaga(self):
+        for crka in self.geslo:
+            if crka not in self.crke:
+                return False
+        return True
 
-def poraz(self):
-    return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK    
+    def poraz(self):
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK    
 
-def pravilni_del_gesla(self):
-    izpis = ""
-    for crka in self.geslo:
-        if crka in self.pravilne_crke():
-            izpis += crka
-        else:
-            izpis += '_'
-    return izpis
+    def pravilni_del_gesla(self):
+        izpis = ""
+        for crka in self.geslo:
+            if crka in self.pravilne_crke():
+                izpis += crka
+            else:
+                izpis += '_'
+        return izpis
 
-def nepravilni_ugibi(self):
-    return ' '.join(self.napacne_crke)
+    def nepravilni_ugibi(self):
+        return ' '.join(self.napacne_crke())
 
-def ugibaj(self, crka):
-    velika_crka = crka.upper()
-    if velika_crka in self.crke:
-        return PONOVLJENA_CRKA
-    self.crke.append(velika_crka)
-    if self.zmaga():
-        return ZMAGA
-    elif self.poraz():
-        return PORAZ
-    else: 
-        if velika_crka in self.pravilne_crke():
-            return PRAVILNA_CRKA
-        elif velika_crka in self.napacne_crke():
-            return NAPACNA_CRKA
+    def ugibaj(self, crka):
+        velika_crka = crka.upper()
+        if velika_crka in self.crke:
+            return PONOVLJENA_CRKA
+        self.crke.append(velika_crka)
+        if self.zmaga():
+            return ZMAGA
+        elif self.poraz():
+            return PORAZ
+        else: 
+            if velika_crka in self.pravilne_crke():
+                return PRAVILNA_CRKA
+            elif velika_crka in self.napacne_crke():
+                return NAPACNA_CRKA
 
 #definirali smo razred - naredili smo model 
 
@@ -68,4 +68,4 @@ with open('besede.txt') as f:
 def nova_igra():
     import random
     izbrana_beseda = random.choice(bazen_besed)
-    return igra = Igra(izbrana_beseda)
+    return Igra(izbrana_beseda)
--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,4 +1,4 @@
-import model.py
+import model
 
 def izpis_igre(igra):
     return """================================
@@ -8,29 +8,30 @@ Ugibaš še: {stevilo}.krat
 ===================================""" .format(
     geslo = igra.pravilni_del_gesla(),
     napacne_crke = igra.nepravilni_ugibi(),
-    stevočp = model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo
+    stevilo = model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak()
 )
 
 def izpis_zmage(igra):
-    return "Čestitke uganil/a si geslo {}.".format(igra)
+    return "Čestitke uganil/a si geslo {}.".format(igra.geslo)
 
 def izpis_poraza(igra):
     return "Več sreče prihodnjič!"
 
-def zahtevaj_vnos(igra):
+def zahtevaj_vnos():
     return input ("Ugibaj: ")
 
 def pozeni_vmesnik():
     igra = model.nova_igra()
-    print(izpis_igre(igra))
-    crka = zahtevaj_vnos()
-    stanje = igra.ugibaj(crka)
-    if stanje == model.ZMAGA:
-        print(izpis_zmage(igra))
-        break
-    elif stanje == model.PORAZ:
-        print(izpis_poraza(igra))
-        break
+    while True:
+        print(izpis_igre(igra))
+        crka = zahtevaj_vnos()
+        stanje = igra.ugibaj(crka)
+        if stanje == model.ZMAGA:
+            print(izpis_zmage(igra))
+            break
+        elif stanje == model.PORAZ:
+            print(izpis_poraza(igra))
+            break
     
 pozeni_vmesnik()
 
