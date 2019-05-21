98465e35c8a6846646e859eb33c30acb9696a38f
Klara Širca <klara.sirca@student.fmf.uni-lj.si>
2019-05-21 19:57:01 +0200

    naredili vaje

--- vislice.py
+++ vislice.py
@@ -16,18 +16,18 @@ def slike(ime):
 @bottle.get('/igra/')
 def nova_igra():
         id = vislice.nova_igra()
-        bottle.redirect('/igra{0}/'.format(id))
+        bottle.redirect('/igra/{0}/'.format(id))
 
 @bottle.get('/igra/<id_igre:int>/')
 def pokazi_igro(id_igre):
-    igra, stanje = vislice.igre(id_igre)
+    igra, stanje = vislice.igre[id_igre]
     return bottle.template('igra.html', id_igre = id_igre , igra=igra, stanje = stanje)
 
 @bottle.post('/igra/<id_igre:int>/')
 def ugibaj(id_igre):
     crka = bottle.request.forms.getunicode('crka')
     vislice.ugibaj(id_igre, crka)
-    bottle.redirect('/igra{0}/'.format(id_igre))
+    bottle.redirect('/igra/{0}/'.format(id_igre))
 
 
-    bottle.run(reloader = True, debug = True)
+bottle.run(reloader = True, debug = True)
