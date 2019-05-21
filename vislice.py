cc9de05dfb6e307d50b6d4f8ba1c434d3469807d
Klara Širca <klara.sirca@student.fmf.uni-lj.si>
2019-05-21 19:52:02 +0200

    naredili spletni

--- vislice.py
+++ vislice.py
@@ -2,22 +2,32 @@ import bottle
 import model
 
 vislice = model.Vislice()
-id = vislice.nova_igra()
-igra, stanje = vislice.igre[id]
 
 @bottle.get('/')
 def index():
     return bottle.template('index.tpl')
     #morajo biti v views mapici
 
-@bottle.get('/igra/')
-def testigra():
-    return bottle.template('igra.html', id_igra = id, igra=igra, stanje = stanje)
-    #morajo biti v views mapici
-
 @bottle.get('/img/<ime>')
 def slike(ime):
     return bottle.static_file(ime, root = 'img')
 
-bottle.run(reloader = True, debug = True)
 
+@bottle.get('/igra/')
+def nova_igra():
+        id = vislice.nova_igra()
+        bottle.redirect('/igra{0}/'.format(id))
+
+@bottle.get('/igra/<id_igre:int>/')
+def pokazi_igro(id_igre):
+    igra, stanje = vislice.igre(id_igre)
+    return bottle.template('igra.html', id_igre = id_igre , igra=igra, stanje = stanje)
+
+@bottle.post('/igra/<id_igre:int>/')
+def ugibaj(id_igre):
+    crka = bottle.request.forms.getunicode('crka')
+    vislice.ugibaj(id_igre, crka)
+    bottle.redirect('/igra{0}/'.format(id_igre))
+
+
+    bottle.run(reloader = True, debug = True)
